4d000d03959f0b5f59e6ab7e8c88bbc0561c8031
fabiocaettano <fabio.caettano74@gmail.com>
2023-05-25 15:09:08 -0300

    lesson parsing request a path, http methodos and querystrings

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Node.js The Master Class
+
+
+curl --request GET --url 'http://localhost:3000/foo'
+curl --request GET --url 'http://localhost:3000/foo?name=fabio&idade=48'
+curl --request GET --url 'http://localhost:3000?name=fabio&idade=48'
--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-var http = require('http');
+/**
+ * 
+ */
 
+//Dependencies
+var http = require('http');
+var url = require('url');
+
+//Configure the server to respond to all requests with a string
 var server = http.createServer(function(req,res){
-    res.end('Hello World \n');
+
+    //Parse the url
+    var parsedUrl = url.parse(req.url, true);
+
+    //Geh the path
+    var path = parsedUrl.pathname;    
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');       
+
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    var method = req.method.toLowerCase();
+
+    
+
+    // Log the request/response
+    console.log('Request received on path: '+trimmedPath);
+    console.log(`Method: ${method}`);
+    console.log(queryStringObject);
+    
+    
+    // control for favicon
+    if (trimmedPath === 'favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end();
+        console.log('favicon requested \n');
+        return;
+    }
+
+    // not the favicon   
+    res.writeHead(200, {'Content-Type': 'text/plain'} );
+    res.write('Hello, world! \n');
+    res.end();     
+
+    
 });
 
 server.listen(3000,function(){
